830fcf6f5d51c4dbec6059fb4a6f4abc9f30e592
Tom McGinn <tommcginn@Toms-Air.lan>
2022-11-25 08:30:43 -0500

    Enabling CORS

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -11,7 +11,17 @@ export function StorageStack({stack, app}) {
         primaryIndex: { partitionKey: "userId", sortKey: "noteId"},
     });
 
-    const bucket = new Bucket(stack, "Uploads");
+    const bucket = new Bucket(stack, "Uploads", {
+        cors: [
+            {
+                maxAge: "1 day",
+                allowedOrigins: ["*"],
+                allowedHeaders: ["*"],
+                allowedMethods: ["GET", "PUT", "POST", "DELETE", "HEAD"],
+
+            },
+        ],
+    });
 
     return {
         table,
